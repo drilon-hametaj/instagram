5f13f21984b20d6a8e7da4193b40eadd6f18bf49
DrilonHametaj <48368584+drilon-hametaj@users.noreply.github.com>
2020-08-17 11:46:05 +0200

    Update login.py

--- function/login.py
+++ function/login.py
@@ -13,10 +13,10 @@ class Login:
         self.driver.find_element_by_xpath('/html/body/div[1]/section/main/article/div/div/div/div[2]/button').click()
         time.sleep(5)
         self.driver.find_element_by_xpath('/html/body/div[1]/section/main/article/div/div/div/form/div[4]/div/label/input') \
-            .send_keys('mindset__vincente') #username
+            .send_keys('nome') #username
         time.sleep(2)
         self.driver.find_element_by_xpath('/html/body/div[1]/section/main/article/div/div/div/form/div[5]/div/label/input') \
-            .send_keys('Chepalle1') #password
+            .send_keys('password') #password
         time.sleep(5)
         try:
             self.driver.find_element_by_xpath('/html/body/div[1]/section/main/article/div/div/div/form/div[7]/button').click()
